10e0572e2e1635c8423ba0c6b1c7c5d5d506eb74
jeff <jeff@rappelkiste.(none)>
2013-02-04 00:56:35 +0100

    default to OSciMapV2

--- TileMapApp/src/org/oscim/app/TileMap.java
+++ TileMapApp/src/org/oscim/app/TileMap.java
@@ -150,12 +150,12 @@ public class TileMap extends MapActivity implements MapEventsReceiver {
 	private void setMapDatabase(SharedPreferences preferences) {
 		MapDatabases mapDatabaseNew;
 		String dbname = preferences.getString("mapDatabase",
-				MapDatabases.PBMAP_READER.name());
+				MapDatabases.OSCIMAP_READER.name());
 
 		try {
 			mapDatabaseNew = MapDatabases.valueOf(dbname);
 		} catch (IllegalArgumentException e) {
-			mapDatabaseNew = MapDatabases.PBMAP_READER;
+			mapDatabaseNew = MapDatabases.OSCIMAP_READER;
 		}
 
 		if (mapDatabaseNew != mMapDatabase) {
@@ -532,7 +532,6 @@ public class TileMap extends MapActivity implements MapEventsReceiver {
 
 		if (preferences.contains("mapDatabase")) {
 			setMapDatabase(preferences);
-
 		}
 		if (preferences.contains("theme")) {
 			String name = preferences.getString("theme",
