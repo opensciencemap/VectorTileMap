fce109a60add07dff3677a2196634e7bb48cc9ce
hjanetzek <hannes.janetzek@gmail.com>
2013-03-09 16:30:23 +0100

    make getStartPosition() public

--- TileMapExample/src/org/ocsim/test/MapActivity.java
+++ TileMapExample/src/org/ocsim/test/MapActivity.java
@@ -4,8 +4,6 @@ import org.oscim.core.GeoPoint;
 import org.oscim.core.MapPosition;
 import org.oscim.database.MapDatabases;
 import org.oscim.database.MapOptions;
-import org.oscim.overlay.GenericOverlay;
-import org.oscim.renderer.overlays.GridOverlay;
 import org.oscim.view.MapView;
 
 import android.os.Bundle;
@@ -23,24 +21,35 @@ public class MapActivity extends org.oscim.view.MapActivity {
 
 		mMap = (MapView) findViewById(R.id.mapView);
 
-		//MapOptions options = new MapOptions(MapDatabases.PBMAP_READER);
-		//options.put("url", "http://city.informatik.uni-bremen.de:80/osmstache/test/");
-		
-		//MapOptions options = new MapOptions(MapDatabases.OSCIMAP_READER);
-		//options.put("url", "http://city.informatik.uni-bremen.de:80/osci/map-live/");
+		// MapOptions options = new MapOptions(MapDatabases.PBMAP_READER);
+		// options.put("url",
+		// "http://city.informatik.uni-bremen.de:80/osmstache/test/");
+
+		// MapOptions options = new MapOptions(MapDatabases.OSCIMAP_READER);
+		// options.put("url",
+		// "http://city.informatik.uni-bremen.de:80/osci/map-live/");
 
 		MapOptions options = new MapOptions(MapDatabases.MAP_READER);
-		options.put("file", Environment.getExternalStorageDirectory().getPath() +"/bremen.map");
+		// options.put("file",
+		// Environment.getExternalStorageDirectory().getPath() +"/bremen.map");
+		options.put("file", Environment.getExternalStorageDirectory().getPath()
+				+ "/Download/berlin.map");
 
-		
 		mMap.setMapDatabase(options);
 
 		// configure the MapView and activate the zoomLevel buttons
 		mMap.setClickable(true);
 		mMap.setFocusable(true);
-		
-		mMap.setMapCenter(new MapPosition(new GeoPoint(53.1f, 8.8f),(byte)12, 1));
-		//mMap.getOverlayManager().add(new GenericOverlay(mMap,new GridOverlay(mMap)));
+
+		MapPosition mapCenter = mMap.getMapFileCenter();
+		if (mapCenter == null)
+			mMap.setMapCenter(new MapPosition(new GeoPoint(53.1f, 8.8f),
+					(byte) 12, 1));
+		else
+			mMap.setMapCenter(mapCenter);
+
+		// mMap.getOverlayManager().add(new GenericOverlay(mMap,new
+		// GridOverlay(mMap)));
 	}
 
 	@Override
